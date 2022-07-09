d19f5a0446468936a32358f03897d0b7ee37e6f3
whtdoumean <whtdoumean0_0@outlook.com>
2022-07-09 12:10:10 +0300

    work 7

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,27 @@ function myFunction() {
         x.className = "head-nav";
     }
 }
+
+let lastScroll = 0;
+const defoultOffset = 200;
+const header = document.querySelector('.header');
+
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains('hide');
+
+window.addEventListener('scroll', ()=>{
+
+if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defoultOffset) {
+    //scroll down
+    console.log('down');
+    header.classList.add('hide');
+}
+else if (scrollPosition() < lastScroll && containHide()){
+    //scroll up
+    console.log('up');
+    header.classList.remove('hide');
+}
+
+lastScroll  = scrollPosition();
+
+});
